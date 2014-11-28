51c9e1d489730d57bb61936986d9dcfac5e40226
Cecys12 <juliocesarjimenezsalazar@gmail>
2014-11-27 21:12:53 -0600

    Proyecto Final
    Se encuentra los proyectos de MeHasVisto y Proyecto de Trabajadores(CSV)
    y con sus respectivos Reportes

---
+++ DAW/MeHasVisto1/Controllers/PetController.cs
@@ -0,0 +1,69 @@
+﻿using MeHasVisto1.Models;
+using MeHasVisto1.Models.Bussines;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHasVisto1.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public ActionResult Display()
+        {
+            var name = (string)RouteData.Values["id"];
+            string model = null; //PetManagement.GetByName(name);
+            if (model == null)
+                return RedirectToAction("NotFound");
+
+            return View(model);
+        }
+
+        public FileResult DownLoadPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture = "/Content/Uploads/" + name + ".jpg"; //ruta
+            var contentType = "image/jpg"; //tipo de identificador
+            var fileName = name + ".jpg";
+            return File(picture, contentType, fileName);
+        }
+        public ActionResult NotFound()
+        {
+            ViewBag.ErrorCode = "NFE0001";
+            ViewBag.Description = "La mascota no se encuentra" +
+                "En la base de datos";
+            return View();
+        }
+
+        public HttpStatusCodeResult UnauthorizedError()
+        {
+            return new HttpUnauthorizedResult("Nada por aqui");
+            
+        }
+        // Action method  que permitira la carga
+        // la foto de una mascota
+        public ActionResult PictureUpload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PictureUpload(PictureModel model)
+        {
+            if (model.PictureFile.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(model.PictureFile.FileName);
+                var filePath = Server.MapPath("/Content/Uploads");
+                string savedFileName = Path.Combine(filePath, fileName);
+                model.PictureFile.SaveAs(savedFileName);
+                PetManagement.CreateThumbnail(fileName, filePath, 100, 100, true);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ proyecto de trabajadores/Recover/EmployeQuiz/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeQuiz.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+           public ActionResult NotFoundError()
+        {
+            return HttpNotFound("Trabajador No Encontrad@...");
+
+        }
+           public ActionResult NotFound()
+           {
+               ViewBag.ErrorCode = "NFE0001";
+               ViewBag.Description = "El trabajador@ no se encuentra" + "En nuestra BD";
+               return View();
+           }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
